a7409e94840f59b9da94dde03ea025088b6a543e
Asier Belloso <asierbelloso29@opendeusto.es>
2022-11-09 19:56:35 +0100

    Change DB

--- mysite/patata/models.py
+++ mysite/patata/models.py
@@ -2,18 +2,25 @@ from django.db import models
 
 class Genero(models.Model):
     nombre = models.CharField(max_length= 50)
+    def __str__(self) -> str:
+        return self.nombre
 
 class Plataforma(models.Model):
     nombre = models.CharField(max_length=50)
     owner = models.CharField(max_length=50)
     url_p = models.TextField(default = "")
+    def __str__(self) -> str:
+        return self.nombre
 
 class Videojuego(models.Model):
     nombre = models.CharField(max_length=50)
-    fecha_salida = models.DateField()
+    fecha_salida = models.IntegerField()
     pegi = models.IntegerField()
     comanya = models.CharField(max_length=50)
     url_v = models.TextField(default = "")
 
     genero = models.ForeignKey(Genero, on_delete=models.CASCADE)
     plataforma = models.ManyToManyField(Plataforma)
+
+    def __str__(self) -> str:
+        return self.nombre
