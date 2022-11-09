b6dbf6abfee4837a4d9c8b0c2bfce0df2e585167
a-rasines <alberto.rasines@opendeusto.es>
2022-11-09 20:10:47 +0100

    Cosas natxis

--- mysite/patata/views.py
+++ mysite/patata/views.py
@@ -2,11 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+
+from .models import *
+
+
 def index(request):
  return HttpResponse("Hello, world!")
 
 def home(request):
-    return render(request, 'static/index.html', {})
+    vars = {
+        'plataformas' : Plataforma.objects.all()[0:4],
+    }
+    return render(request, 'static/index.html', vars)
 
 def contact(request):
     return render(request, 'static/contact.html', {})
