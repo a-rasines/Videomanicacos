84bea7e4fee08c66a4aeeb4a1fb6ffa32897cf5f
a-rasines <alberto.rasines@opendeusto.es>
2022-11-09 17:59:46 +0100

    Template

--- mysite/patata/views.py
+++ mysite/patata/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
  return HttpResponse("Hello, world!")
+
+def home(request):
+    return render(request, 'static/index.html', {})
