a5c6d26f8e64ca0ae5be7c828daed6b4ebafb6f3
Asier Belloso <asierbelloso29@opendeusto.es>
2022-11-09 19:18:34 +0100

    Admin y mas

--- mysite/patata/admin.py
+++ mysite/patata/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Videojuego)
+admin.site.register(Genero)
+admin.site.register(Plataforma)
--- mysite/patata/models.py
+++ mysite/patata/models.py
@@ -6,12 +6,14 @@ class Genero(models.Model):
 class Plataforma(models.Model):
     nombre = models.CharField(max_length=50)
     owner = models.CharField(max_length=50)
+    url_p = models.TextField(default = "")
 
 class Videojuego(models.Model):
     nombre = models.CharField(max_length=50)
     fecha_salida = models.DateField()
     pegi = models.IntegerField()
     comanya = models.CharField(max_length=50)
+    url_v = models.TextField(default = "")
 
     genero = models.ForeignKey(Genero, on_delete=models.CASCADE)
     plataforma = models.ManyToManyField(Plataforma)
